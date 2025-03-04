dccf675eec7de15b47b8fd53e3c7c27e231551ba
Zip <thantzinhtet2001@gmail.com>
2025-03-05 02:50:49 +0630

    Home Slider and footer done

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,14 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform, StyleSheet } from "react-native";
+import { HapticTab } from "@/components/HapticTab";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import Svg, {
+  Path,
+  Defs,
+  LinearGradient as SvgLinearGradient,
+  Stop,
+} from "react-native-svg";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -14,32 +16,175 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: "#000",
+        tabBarInactiveTintColor: "#CFCFCF",
         headerShown: false,
         tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+        tabBarStyle: {
+          ...styles.tabBar,
+          ...Platform.select({
+            ios: { position: "absolute" },
+          }),
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Home",
+          tabBarIcon: ({ focused, color }) => (
+            <Svg width={24} height={28} viewBox="0 0 24 28" fill="none">
+              <Defs>
+                {focused && (
+                  <SvgLinearGradient
+                    id="paint0_linear_68_279"
+                    x1="12"
+                    y1="0"
+                    x2="12"
+                    y2="28"
+                  >
+                    <Stop stopColor="#52C5FE" />
+                    <Stop offset="1" stopColor="#285CE8" />
+                  </SvgLinearGradient>
+                )}
+              </Defs>
+              <Path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M0.789919 9.30147C0.333252 10.2965 0.333252 11.4281 0.333252 13.6915V20.7581C0.333252 23.9031 0.333252 25.4748 1.30992 26.4498C2.19492 27.3365 3.57159 27.4165 6.16658 27.4248V19.0915C6.16658 18.2074 6.51778 17.3596 7.1429 16.7344C7.76802 16.1093 8.61586 15.7581 9.49992 15.7581H14.4999C15.384 15.7581 16.2318 16.1093 16.8569 16.7344C17.4821 17.3596 17.8333 18.2074 17.8333 19.0915V27.4248C20.4283 27.4165 21.8049 27.3348 22.6899 26.4481C23.6666 25.4715 23.6666 23.9015 23.6666 20.7581V13.6915C23.6666 11.4281 23.6666 10.2965 23.2099 9.30147C22.7516 8.30647 21.8933 7.57147 20.1749 6.09814L18.5083 4.66981C15.3999 2.00814 13.8499 0.674805 11.9999 0.674805C10.1499 0.674805 8.59825 2.00647 5.49159 4.66814L3.82492 6.09647C2.10825 7.56981 1.24825 8.3048 0.791585 9.2998M14.4999 27.4248V19.0915H9.49992V27.4248H14.4999Z"
+                fill={focused ? "url(#paint0_linear_68_279)" : "#CFCFCF"}
+              />
+            </Svg>
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="catalog"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Catalog",
+          tabBarIcon: ({ focused, color }) => (
+            <Svg width={24} height={24} viewBox="0 0 24 24" fill="none">
+              <Defs>
+                {focused && (
+                  <SvgLinearGradient
+                    id="paint0_linear_catalog"
+                    x1="12"
+                    y1="0"
+                    x2="12"
+                    y2="24"
+                  >
+                    <Stop stopColor="#52C5FE" />
+                    <Stop offset="1" stopColor="#285CE8" />
+                  </SvgLinearGradient>
+                )}
+              </Defs>
+              <Path
+                d="M22.1333 24L13.7333 15.6C13.0667 16.1333 12.3 16.5556 11.4333 16.8667C10.5667 17.1778 9.64445 17.3333 8.66667 17.3333C6.24445 17.3333 4.19467 16.4942 2.51733 14.816C0.840001 13.1378 0.000889594 11.088 7.05467e-07 8.66667C-0.000888183 6.24533 0.838223 4.19556 2.51733 2.51733C4.19645 0.839111 6.24622 0 8.66667 0C11.0871 0 13.1373 0.839111 14.8173 2.51733C16.4973 4.19556 17.336 6.24533 17.3333 8.66667C17.3333 9.64444 17.1778 10.5667 16.8667 11.4333C16.5556 12.3 16.1333 13.0667 15.6 13.7333L24 22.1333L22.1333 24ZM8.66667 14.6667C10.3333 14.6667 11.7502 14.0836 12.9173 12.9173C14.0844 11.7511 14.6676 10.3342 14.6667 8.66667C14.6658 6.99911 14.0827 5.58267 12.9173 4.41733C11.752 3.252 10.3351 2.66844 8.66667 2.66667C6.99822 2.66489 5.58178 3.24844 4.41733 4.41733C3.25289 5.58622 2.66933 7.00267 2.66667 8.66667C2.664 10.3307 3.24756 11.7476 4.41733 12.9173C5.58711 14.0871 7.00356 14.6702 8.66667 14.6667Z"
+                fill={focused ? "url(#paint0_linear_catalog)" : "#CFCFCF"}
+              />
+            </Svg>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="favorites"
+        options={{
+          title: "Favorites",
+          tabBarIcon: ({ focused }) => (
+            <Svg width={28} height={25} viewBox="0 0 28 25" fill="none">
+              <Defs>
+                {focused && (
+                  <SvgLinearGradient
+                    id="activeGradientFavorites"
+                    x1="14"
+                    y1="0"
+                    x2="14"
+                    y2="25"
+                  >
+                    <Stop stopColor="#52C5FE" />
+                    <Stop offset="1" stopColor="#285CE8" />
+                  </SvgLinearGradient>
+                )}
+              </Defs>
+              <Path
+                d="M14.0001 24.9999L12.0668 23.2665C9.82231 21.2443 7.96675 19.4999 6.50008 18.0332C5.03342 16.5665 3.86675 15.2496 3.00008 14.0825C2.13342 12.9154 1.52808 11.8434 1.18408 10.8665C0.840081 9.88965 0.667637 8.88965 0.666748 7.86654C0.666748 5.77765 1.36675 4.0332 2.76675 2.6332C4.16675 1.2332 5.91119 0.533203 8.00008 0.533203C9.15564 0.533203 10.2556 0.777647 11.3001 1.26654C12.3445 1.75543 13.2445 2.44431 14.0001 3.3332C14.7556 2.44431 15.6556 1.75543 16.7001 1.26654C17.7445 0.777647 18.8445 0.533203 20.0001 0.533203C22.089 0.533203 23.8334 1.2332 25.2334 2.6332C26.6334 4.0332 27.3334 5.77765 27.3334 7.86654C27.3334 8.88876 27.1614 9.88876 26.8174 10.8665C26.4734 11.8443 25.8676 12.9163 25.0001 14.0825C24.1325 15.2488 22.9659 16.5656 21.5001 18.0332C20.0343 19.5008 18.1788 21.2452 15.9334 23.2665L14.0001 24.9999ZM14.0001 21.3999C16.1334 19.4888 17.889 17.8501 19.2668 16.4839C20.6445 15.1176 21.7334 13.9288 22.5334 12.9172C23.3334 11.9056 23.889 11.0052 24.2001 10.2159C24.5112 9.42654 24.6668 8.64342 24.6668 7.86654C24.6668 6.5332 24.2223 5.42209 23.3334 4.5332C22.4445 3.64431 21.3334 3.19987 20.0001 3.19987C18.9556 3.19987 17.989 3.49409 17.1001 4.08254C16.2112 4.67098 15.6001 5.4212 15.2668 6.3332H12.7334C12.4001 5.42209 11.789 4.67231 10.9001 4.08387C10.0112 3.49543 9.04453 3.20076 8.00008 3.19987C6.66675 3.19987 5.55564 3.64431 4.66675 4.5332C3.77786 5.42209 3.33342 6.5332 3.33342 7.86654C3.33342 8.64431 3.48897 9.42787 3.80008 10.2172C4.11119 11.0065 4.66675 11.9065 5.46675 12.9172C6.26675 13.9279 7.35564 15.1168 8.73342 16.4839C10.1112 17.851 11.8668 19.4896 14.0001 21.3999Z"
+                fill={focused ? "url(#activeGradientFavorites)" : "#CFCFCF"}
+              />
+            </Svg>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="cart"
+        options={{
+          title: "Cart",
+          tabBarIcon: ({ focused }) => (
+            <Svg width={27} height={28} viewBox="0 0 27 28" fill="none">
+              <Defs>
+                {focused && (
+                  <SvgLinearGradient
+                    id="activeGradientCart"
+                    x1="13.5"
+                    y1="0"
+                    x2="13.5"
+                    y2="28"
+                  >
+                    <Stop stopColor="#52C5FE" />
+                    <Stop offset="1" stopColor="#285CE8" />
+                  </SvgLinearGradient>
+                )}
+              </Defs>
+              <Path
+                d="M21.6666 21.9998C20.1866 21.9998 18.9999 23.1865 18.9999 24.6665C18.9999 25.3738 19.2809 26.052 19.781 26.5521C20.2811 27.0522 20.9593 27.3332 21.6666 27.3332C22.3738 27.3332 23.0521 27.0522 23.5522 26.5521C24.0523 26.052 24.3333 25.3738 24.3333 24.6665C24.3333 23.9593 24.0523 23.281 23.5522 22.7809C23.0521 22.2808 22.3738 21.9998 21.6666 21.9998ZM0.333252 0.666504V3.33317H2.99992L7.79992 13.4532L5.98659 16.7198C5.78659 17.0932 5.66659 17.5332 5.66659 17.9998C5.66659 18.7071 5.94754 19.3854 6.44764 19.8855C6.94773 20.3856 7.62601 20.6665 8.33325 20.6665H24.3333V17.9998H8.89326C8.80485 17.9998 8.72007 17.9647 8.65755 17.9022C8.59504 17.8397 8.55992 17.7549 8.55992 17.6665C8.55992 17.5998 8.57325 17.5465 8.59992 17.5065L9.79992 15.3332H19.7333C20.7333 15.3332 21.6133 14.7732 22.0666 13.9598L26.8399 5.33317C26.9333 5.11984 26.9999 4.89317 26.9999 4.66651C26.9999 4.31288 26.8595 3.97374 26.6094 3.7237C26.3594 3.47365 26.0202 3.33317 25.6666 3.33317H5.94659L4.69325 0.666504M8.33325 21.9998C6.85325 21.9998 5.66659 23.1865 5.66659 24.6665C5.66659 25.3738 5.94754 26.052 6.44764 26.5521C6.94773 27.0522 7.62601 27.3332 8.33325 27.3332C9.0405 27.3332 9.71878 27.0522 10.2189 26.5521C10.719 26.052 10.9999 25.3738 10.9999 24.6665C10.9999 23.9593 10.719 23.281 10.2189 22.7809C9.71878 22.2808 9.0405 21.9998 8.33325 21.9998Z"
+                fill={focused ? "url(#activeGradientCart)" : "#CFCFCF"}
+              />
+            </Svg>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: "Account",
+          tabBarIcon: ({ focused }) => (
+            <Svg width={22} height={22} viewBox="0 0 22 22" fill="none">
+              <Defs>
+                {focused && (
+                  <SvgLinearGradient
+                    id="activeGradientAccount"
+                    x1="11"
+                    y1="0"
+                    x2="11"
+                    y2="22"
+                  >
+                    <Stop stopColor="#52C5FE" />
+                    <Stop offset="1" stopColor="#285CE8" />
+                  </SvgLinearGradient>
+                )}
+              </Defs>
+              <Path
+                d="M10.9999 0.333496C12.4144 0.333496 13.771 0.895399 14.7712 1.89559C15.7713 2.89579 16.3333 4.25234 16.3333 5.66683C16.3333 7.08132 15.7713 8.43787 14.7712 9.43807C13.771 10.4383 12.4144 11.0002 10.9999 11.0002C9.58543 11.0002 8.22888 10.4383 7.22868 9.43807C6.22849 8.43787 5.66659 7.08132 5.66659 5.66683C5.66659 4.25234 6.22849 2.89579 7.22868 1.89559C8.22888 0.895399 9.58543 0.333496 10.9999 0.333496ZM10.9999 13.6668C16.8933 13.6668 21.6666 16.0535 21.6666 19.0002V21.6668H0.333252V19.0002C0.333252 16.0535 5.10659 13.6668 10.9999 13.6668Z"
+                fill={focused ? "url(#activeGradientAccount)" : "#CFCFCF"}
+              />
+            </Svg>
+          ),
         }}
       />
     </Tabs>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  tabBar: {
+    backgroundColor: "#fff",
+    boxShadow: "0px -4px 10px 0px #00000014",
+    height: 70,
+    paddingTop: 10,
+    borderColor: "#fff",
+    position: "fixed",
+    bottom: 0,
+  },
+});
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,159 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient"; // For Expo projects
+import Svg, { ClipPath, Defs, G, Path, Rect } from "react-native-svg";
+import Carousel from "react-native-reanimated-carousel";
 
 export default function HomeScreen() {
+  const { width } = Dimensions.get("window");
+
+  const carouselData = [
+    {
+      id: "1",
+      image:
+        "https://s3-alpha-sig.figma.com/img/5701/eaa6/7957e114a49c57d80fb2842e5294598f?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=uCXV1lt8BkxI3kok5PTNYzR07nkerlFJcZTm71PkG75NzQhl1mpeySTcLraSfLgOKzaeScBEohu3wOwKHhvBhGNf0gKAZ~9wZC0gsM5gNM3WeKzsiIKbAndy7g71gXXS6ErdYnbT4XaJ-cc5fT5MEoCbOpSBOmxdHVD7bi5MlzWZiHyB7o7~GaAuHM-ooxikOEwsK-qBbJSM8KtlhOeaSoIHWptY~modV~cJFH9vVs006xHh8qoBm5Xk0moJSHzQ9nU2RaG-N4PhkFk-Qj0mrINTBukxWO6u2WsEa134XHu7twzIDeudWxNgiiDDzQkesEN-NpqOzK-omfe6u5XMQw__",
+      bgContain: true,
+    },
+    {
+      id: "2",
+      image:
+        "https://s3-alpha-sig.figma.com/img/5701/eaa6/7957e114a49c57d80fb2842e5294598f?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=uCXV1lt8BkxI3kok5PTNYzR07nkerlFJcZTm71PkG75NzQhl1mpeySTcLraSfLgOKzaeScBEohu3wOwKHhvBhGNf0gKAZ~9wZC0gsM5gNM3WeKzsiIKbAndy7g71gXXS6ErdYnbT4XaJ-cc5fT5MEoCbOpSBOmxdHVD7bi5MlzWZiHyB7o7~GaAuHM-ooxikOEwsK-qBbJSM8KtlhOeaSoIHWptY~modV~cJFH9vVs006xHh8qoBm5Xk0moJSHzQ9nU2RaG-N4PhkFk-Qj0mrINTBukxWO6u2WsEa134XHu7twzIDeudWxNgiiDDzQkesEN-NpqOzK-omfe6u5XMQw__",
+      bgContain: true,
+    },
+    {
+      id: "3",
+      image:
+        "https://s3-alpha-sig.figma.com/img/5701/eaa6/7957e114a49c57d80fb2842e5294598f?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=uCXV1lt8BkxI3kok5PTNYzR07nkerlFJcZTm71PkG75NzQhl1mpeySTcLraSfLgOKzaeScBEohu3wOwKHhvBhGNf0gKAZ~9wZC0gsM5gNM3WeKzsiIKbAndy7g71gXXS6ErdYnbT4XaJ-cc5fT5MEoCbOpSBOmxdHVD7bi5MlzWZiHyB7o7~GaAuHM-ooxikOEwsK-qBbJSM8KtlhOeaSoIHWptY~modV~cJFH9vVs006xHh8qoBm5Xk0moJSHzQ9nU2RaG-N4PhkFk-Qj0mrINTBukxWO6u2WsEa134XHu7twzIDeudWxNgiiDDzQkesEN-NpqOzK-omfe6u5XMQw__",
+      bgContain: true,
+    },
+  ];
+
+  const renderCarouselItem = ({ item }: any) => {
+    return <Image source={{ uri: item.image }} style={styles.carouselImage} />;
+  };
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <ScrollView style={styles.container}>
+      <LinearGradient
+        colors={["#53CAFE", "#2555E7"]}
+        start={{ x: 0.0, y: 0.0 }}
+        end={{ x: 1.0, y: 0.0 }}
+        style={styles.banner}
+      >
+        <View style={styles.row}>
+          <Image
+            source={{
+              uri: "https://s3-alpha-sig.figma.com/img/866d/ff79/56041d2614d60e18b56b870aaf94a1c4?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=gqpc3MRztAouxFDWqP3tEZvDPEkZzNfvDqc687d5VX3LaBYQR2NOiZwVcLeTCKYU1RI5FrngvHLkcTAwokbclVMxqd-omle3yhOWX1OHwqNsQ6YQXquwsur1sRw1udKcREF5z3Si2OsFR13B9l1sNLV7H-b~UdIbvxAbB05ReqbgPnXzhnxUbEG5oXp9YdwVqeorFqOCm3pEe17gdVtVK1u8cX9gIF2xxghvVAEMvFht7p8EPGYh5p1sepvNQ6Ei6mf-k-s0sHpCnwrRDFAkOwJ8vkHZyFMHNQ09k02iVWWJedKd4pr9tuxbErdYr0Amjv9howSs1wGuTOejQEIWlw__",
+            }}
+            style={styles.avatar}
+          />
+          <View style={styles.center}>
+            <Text style={styles.name}>Thant Zin</Text>
+            <Text style={styles.address}>No. 123, 4th Cross, 5th Main</Text>
+          </View>
+          <TouchableOpacity style={styles.notificationButton}>
+            <Svg width={30} height={30} viewBox="0 0 30 30" fill="none">
+              <Defs>
+                <ClipPath id="clip0_68_292">
+                  <Rect width={30} height={30} fill="white" />
+                </ClipPath>
+              </Defs>
+              <G clipPath="url(#clip0_68_292)">
+                <Path
+                  d="M15 5.625C15 5.275 14.725 5 14.375 5C14.025 5 13.75 5.275 13.75 5.625V7.5375C10.9375 7.85 8.75 10.225 8.75 13.125V20.5125L6.7625 22.5H21.9875L20 20.5125V13.125C20 10.225 17.8125 7.85 15 7.5375V5.625ZM14.375 3.75C15.4125 3.75 16.25 4.5875 16.25 5.625V6.5125C19.1375 7.325 21.25 10 21.25 13.125V20L25 23.75H3.75L7.5 20V13.125C7.5 10 9.6125 7.325 12.5 6.5125V5.625C12.5 4.5875 13.3375 3.75 14.375 3.75ZM14.375 27.5C12.8625 27.5 11.6 26.425 11.3125 25H12.6125C12.8625 25.725 13.5625 26.25 14.375 26.25C15.1875 26.25 15.8875 25.725 16.1375 25H17.4375C17.15 26.425 15.8875 27.5 14.375 27.5Z"
+                  fill="black"
+                />
+              </G>
+            </Svg>
+          </TouchableOpacity>
+        </View>
+
+        {/* Carousel Container */}
+        <View style={styles.carouselContainer}>
+          <Carousel
+            loop
+            width={width}
+            height={180}
+            data={carouselData}
+            renderItem={renderCarouselItem}
+            autoPlay={true}
+            autoPlayInterval={5000}
+          />
+        </View>
+      </LinearGradient>
+
+      {/* Additional content */}
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  banner: {
+    minHeight: 220,
+    justifyContent: "center",
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    paddingHorizontal: 15,
+    marginBottom: 20,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    justifyContent: "space-between",
+  },
+  avatar: {
+    width: 55,
+    height: 55,
+    borderRadius: 20,
+  },
+  center: {
+    flex: 1,
+    gap: 5,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: "500",
+    color: "#fff",
+    fontFamily: "Saira-Medium",
+  },
+  address: {
+    fontSize: 12,
+    color: "#FFFFFFCC",
+    fontFamily: "Saira-Regular",
+  },
+  notificationButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 50,
+    height: 50,
+    borderRadius: 100,
+    backgroundColor: "#FFFFFFB2",
+  },
+  carouselContainer: {
+    borderRadius: 30,
+    width: "100%",
+    aspectRatio: "2/1",
+    alignSelf: "center",
+    overflow: "hidden",
+    marginTop: 30,
+    marginBottom: -150,
+  },
+  carouselImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
   },
 });
