b1f4e18769c33b2b5d3fd1f68dcb39d24517a2d0
Zip <thantzinhtet2001@gmail.com>
2025-03-16 22:10:38 +0630

    Account Page done

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,16 +1,170 @@
-import { ScrollView, StyleSheet, Text } from "react-native";
+import HeadSection from "@/components/Account/HeadSection";
+import { LinearGradient } from "expo-linear-gradient";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Svg, { Path } from "react-native-svg";
 
 export default function Account() {
   return (
-    <ScrollView style={styles.container}>
-      <Text>Account</Text>
-    </ScrollView>
+    <>
+      <LinearGradient
+        colors={["#53CAFE", "#2555E7"]}
+        start={{ x: 0.0, y: 0.0 }}
+        end={{ x: 1.0, y: 0.0 }}
+        style={styles.headLine}
+      ></LinearGradient>
+      <ScrollView style={styles.container}>
+        {/* Head Section Start */}
+        <HeadSection />
+        {/* Head Section End */}
+
+        {/* Body Section End */}
+        <View style={styles.bodyContent}>
+          <Text style={styles.headText}>My Orders</Text>
+
+          <View style={styles.iconList}>
+            <TouchableOpacity style={styles.iconBlock}>
+              <View style={styles.icon}>
+                <Svg width={22} height={27} viewBox="0 0 25 27" fill="none">
+                  <Path
+                    d="M18.25 26.5c-1.73 0-3.203-.61-4.421-1.829C12.61 23.452 12 21.978 12 20.25c0-1.728.609-3.202 1.829-4.421C15.049 14.609 16.523 14 18.25 14c1.727 0 3.202.61 4.422 1.829 1.221 1.219 1.83 2.693 1.828 4.421-.003 1.728-.612 3.203-1.829 4.422-1.216 1.22-2.69 1.83-4.421 1.828zm2.094-3.281l.875-.875L18.875 20v-3.5h-1.25v4l2.719 2.719zM3.25 25.25a2.407 2.407 0 01-1.765-.734A2.412 2.412 0 01.75 22.75V5.25c0-.688.245-1.276.735-1.765A2.413 2.413 0 013.25 2.75h5.219c.229-.73.677-1.328 1.344-1.796A3.732 3.732 0 0112 .25a3.75 3.75 0 012.235.704c.657.469 1.1 1.068 1.328 1.796h5.187c.688 0 1.276.245 1.766.735s.735 1.078.734 1.765v7.813c-.375-.271-.77-.5-1.188-.688a12.235 12.235 0 00-1.312-.5V5.25h-2.5V9H5.75V5.25h-2.5v17.5h6.625c.146.458.313.896.5 1.313.188.416.417.812.688 1.187H3.25zm8.75-20c.354 0 .651-.12.891-.36s.36-.537.359-.89c0-.353-.12-.65-.36-.89s-.536-.36-.89-.36-.65.12-.89.36-.36.537-.36.89.12.65.36.891.538.36.89.359z"
+                    fill="#363636"
+                  />
+                </Svg>
+              </View>
+              <Text style={styles.iconText}>Pending</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.iconBlock}>
+              <View style={styles.icon}>
+                <Svg width={22} height={30} viewBox="0 0 27 30" fill="none">
+                  <Path
+                    d="M19.5 9.375H12V7.5h7.5v1.875zM12 15h7.5v1.875H12V15zm1.919 13.125L15.794 30H.75V0h22.5v18.794l-1.875 1.875V1.875H2.625v26.25h11.294zm-3.135-21.84l-3.472 3.47-2.534-2.533 1.319-1.319L7.312 7.12l2.154-2.153 1.318 1.318zm0 7.5l-3.472 3.47-2.534-2.533 1.319-1.319 1.215 1.216 2.154-2.153 1.318 1.318zm-3.472 8.334l2.154-2.153 1.318 1.318-3.472 3.472-2.534-2.534 1.319-1.319 1.215 1.216zm19.41-.835l-8.16 8.174-3.94-3.955 1.318-1.318 2.623 2.607 6.84-6.826 1.319 1.318z"
+                    fill="#363636"
+                  />
+                </Svg>
+              </View>
+              <Text style={styles.iconText}>Confirmed</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.iconBlock}>
+              <View style={styles.icon}>
+                <Svg width={22} height={25} viewBox="0 0 26 25" fill="none">
+                  <Path
+                    d="M4.5.75C3.875.75 3.375 1 3 1.5l-1.625 2C1 4 .75 4.5.75 5.125V20.75c0 1.375 1.125 2.5 2.5 2.5h10.375c-.25-.75-.375-1.625-.375-2.5 0-4.125 3.375-7.5 7.5-7.5.875 0 1.75.125 2.5.375v-8.5C23.25 4.5 23 4 22.625 3.5l-1.75-2.125C20.625 1 20.125.75 19.5.75h-15zM4.375 2h15L20.5 3.25H3.375l1-1.25zM4.5 15.75H12v3.75H4.5v-3.75zm19.125 1l-4.5 4.5-2-2-1.375 1.5 3.5 3.75 6-6-1.625-1.75z"
+                    fill="#363636"
+                  />
+                </Svg>
+              </View>
+              <Text style={styles.iconText}>Delivered</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.iconBlock}>
+              <View style={styles.icon}>
+                <Svg width={20} height={27} viewBox="0 0 24 27" fill="none">
+                  <Path
+                    d="M10.225 23a8.41 8.41 0 001.075 2.5H2.5A2.491 2.491 0 010 23V3A2.5 2.5 0 012.5.5h15A2.5 2.5 0 0120 3v10.225A8.733 8.733 0 0018.125 13c-.212 0-.413 0-.625.037V3h-6.25v10l-3.125-2.813L5 13V3H2.5v20h7.725zm13.525-1.875c0 3.125-2.5 5.625-5.625 5.625a5.602 5.602 0 01-5.625-5.625C12.5 18 15 15.5 18.125 15.5s5.625 2.5 5.625 5.625zM20 24.35l-5.1-5.1a3.462 3.462 0 00-.525 1.875 3.745 3.745 0 003.75 3.75c.7 0 1.35-.188 1.875-.525zm1.875-3.225a3.745 3.745 0 00-3.75-3.75c-.7 0-1.35.188-1.875.525l5.1 5.1a3.462 3.462 0 00.525-1.875z"
+                    fill="#363636"
+                  />
+                </Svg>
+              </View>
+              <Text style={styles.iconText}>Cancelled</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View>
+            <LinearGradient
+              colors={["#54CAFF", "#275AE8"]}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}
+              style={styles.chat}
+            >
+              <Svg width={22} height={24} viewBox="0 0 26 24" fill="none">
+                <Path
+                  d="M13 .75c6.875 0 12.5 4.475 12.5 10s-5.625 10-12.5 10c-1.55 0-3.037-.225-4.412-.625C4.938 23.25.5 23.25.5 23.25c2.913-2.913 3.375-4.875 3.438-5.625C1.812 15.838.5 13.412.5 10.75c0-5.525 5.625-10 12.5-10z"
+                  fill="#fff"
+                />
+              </Svg>
+              <Text style={styles.chatText}>Chat with Admin</Text>
+            </LinearGradient>
+          </View>
+        </View>
+        {/* Body Section End */}
+      </ScrollView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 15,
+    flex: 1,
     backgroundColor: "#fff",
+  },
+  headLine: {
+    height: 40,
+    position: "fixed",
+    top: 0,
+    zIndex: 100,
+  },
+  bodyContent: {
+    marginTop: 25,
+    gap: 10,
+  },
+  headText: {
+    fontSize: 22,
+    fontWeight: 500,
+    color: "#000000",
+    fontFamily: "Saira-Medium",
+    textAlign: "center",
+  },
+  iconList: {
+    marginTop: 18,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 20,
+  },
+  iconBlock: {
+    gap: 8,
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  icon: {
+    width: 65,
+    height: 65,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 25,
+    backgroundColor: "#F5F5F5",
+  },
+  iconText: {
+    fontSize: 14,
+    fontWeight: 500,
+    color: "#000000",
+    fontFamily: "Saira-Medium",
+  },
+
+  chat: {
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 1000,
+    marginTop: 35,
+    marginHorizontal: 15,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 15,
+  },
+  chatText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "500",
+    fontFamily: "Saira-Medium",
   },
 });
---
+++ components/Account/HeadSection.tsx
@@ -0,0 +1,192 @@
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Svg, { ClipPath, Defs, G, Path, Rect } from "react-native-svg";
+
+// const ImageCarousel: React.FC<lProps> = ({ data }) => {
+const HeadSection = () => {
+  return (
+    <>
+      <LinearGradient
+        colors={["#53CAFE", "#2555E7"]}
+        start={{ x: 0.0, y: 0.0 }}
+        end={{ x: 1.0, y: 0.0 }}
+        style={styles.banner}
+      >
+        <Text style={styles.headText}>Profile</Text>
+        <View style={styles.center}>
+          <Image
+            source={{
+              uri: "https://s3-alpha-sig.figma.com/img/ab78/ba80/99f577a1233d4c10a9529f8a84c9c584?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=dJfu3wy4m7Bl84altipFK-W2ihxyShNYD91Hgi6pqLMpHsxntOqcnzN7triL74lqr8Z~OgNPq1eWzXyl4LeqaYYLkf6X86uHOGOw29JmXdKJ5xdph2U46JX5eZ6y9rZnmWEAWO9sgGwrTiQHyGUjEM14nygQFQ5ZeOT6TKYpeYFgSvwQCurixXJec~sK0GItTI3HSyUNWoi7M8sciz3jkU9im9j0VMzpO7h3omfwJcN-cW~Rd-rqvotmE1CbL0WE~wM1ktvU41ZPiheCnfqH14PRtskjClAblfsAzlBRDivrtVXO-9Qfmghf37lfs1y1ljvcBSwjBYJPxmIXHbqH6w__",
+            }}
+            style={styles.avatar}
+          />
+          <Text style={styles.name}>Thant Zin</Text>
+          <Text style={styles.phone}>09 7920 459 48</Text>
+        </View>
+
+        <View style={styles.amountBar}>
+          <View style={styles.barBlock}>
+            <Svg width={28} height={28} viewBox="0 0 28 28" fill="none">
+              <Path
+                d="M4.625 11.5c0-4.714 0-7.071 1.465-8.535C7.555 1.501 9.911 1.5 14.625 1.5H16.5c4.714 0 7.071 0 8.535 1.465C26.499 4.43 26.5 6.786 26.5 11.5v5c0 4.714 0 7.071-1.465 8.535C23.57 26.499 21.214 26.5 16.5 26.5h-1.875c-4.714 0-7.071 0-8.535-1.465-1.464-1.465-1.465-3.821-1.465-8.535v-5z"
+                stroke="#fff"
+                strokeWidth={1.5}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <Path
+                d="M18.063 11.5v3.125a1.875 1.875 0 103.75 0V14a6.25 6.25 0 10-2.5 5M4.624 6.5H1.5M4.625 14H1.5m3.125 7.5H1.5M18.063 14a2.5 2.5 0 11-5.001 0 2.5 2.5 0 015 0z"
+                stroke="#fff"
+                strokeWidth={1.5}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </Svg>
+            <Text style={styles.barText}>Address Book</Text>
+          </View>
+          <Svg width={1} height={71} viewBox="0 0 1 71" fill="none">
+            <Path stroke="#fff" d="M0.5 2.18557e-8L0.499997 71" />
+          </Svg>
+          <View style={styles.barBlock}>
+            <Text style={styles.barNumber}>75</Text>
+            <Text style={styles.barText}>Order in Total</Text>
+          </View>
+          <Svg width={1} height={71} viewBox="0 0 1 71" fill="none">
+            <Path stroke="#fff" d="M0.5 2.18557e-8L0.499997 71" />
+          </Svg>
+          <View style={styles.barBlock}>
+            <Text style={styles.barNumber}>126</Text>
+            <Text style={styles.barText}>Wishlist</Text>
+          </View>
+        </View>
+
+        <View style={styles.sideBtn}>
+          <TouchableOpacity style={styles.notificationButton}>
+            <Svg width={30} height={30} viewBox="0 0 30 30" fill="none">
+              <Defs>
+                <ClipPath id="clip0_68_292">
+                  <Rect width={30} height={30} fill="white" />
+                </ClipPath>
+              </Defs>
+              <G clipPath="url(#clip0_68_292)">
+                <Path
+                  d="M15 5.625C15 5.275 14.725 5 14.375 5C14.025 5 13.75 5.275 13.75 5.625V7.5375C10.9375 7.85 8.75 10.225 8.75 13.125V20.5125L6.7625 22.5H21.9875L20 20.5125V13.125C20 10.225 17.8125 7.85 15 7.5375V5.625ZM14.375 3.75C15.4125 3.75 16.25 4.5875 16.25 5.625V6.5125C19.1375 7.325 21.25 10 21.25 13.125V20L25 23.75H3.75L7.5 20V13.125C7.5 10 9.6125 7.325 12.5 6.5125V5.625C12.5 4.5875 13.3375 3.75 14.375 3.75ZM14.375 27.5C12.8625 27.5 11.6 26.425 11.3125 25H12.6125C12.8625 25.725 13.5625 26.25 14.375 26.25C15.1875 26.25 15.8875 25.725 16.1375 25H17.4375C17.15 26.425 15.8875 27.5 14.375 27.5Z"
+                  fill="black"
+                />
+              </G>
+            </Svg>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.notificationButton}>
+            <Svg width={27} height={26} viewBox="0 0 27 26" fill="none">
+              <Path
+                d="M23.875 13.825a1.25 1.25 0 010-1.65l1.6-1.8a1.25 1.25 0 00.15-1.463l-2.5-4.325a1.25 1.25 0 00-1.337-.6l-2.35.475A1.25 1.25 0 0118 3.637l-.762-2.287A1.25 1.25 0 0016.05.5h-5a1.25 1.25 0 00-1.25.85l-.7 2.287a1.25 1.25 0 01-1.438.825L5.25 3.987a1.25 1.25 0 00-1.25.6L1.5 8.912a1.25 1.25 0 00.125 1.463l1.587 1.8a1.25 1.25 0 010 1.65l-1.587 1.8a1.25 1.25 0 00-.125 1.462L4 21.413a1.25 1.25 0 001.337.6l2.35-.476a1.25 1.25 0 011.438.826l.762 2.287a1.25 1.25 0 001.25.85h5a1.25 1.25 0 001.188-.85l.762-2.287a1.25 1.25 0 011.438-.825l2.35.474a1.25 1.25 0 001.337-.6l2.5-4.325a1.25 1.25 0 00-.15-1.462l-1.687-1.8zM22.012 15.5l1 1.125-1.6 2.775-1.474-.3a3.75 3.75 0 00-4.313 2.5L15.15 23h-3.2l-.45-1.425a3.75 3.75 0 00-4.313-2.5l-1.475.3-1.625-2.762 1-1.125a3.75 3.75 0 000-5l-1-1.126 1.6-2.75 1.475.3a3.75 3.75 0 004.313-2.5L11.95 3h3.2l.475 1.425a3.75 3.75 0 004.313 2.5l1.475-.3 1.6 2.775-1 1.125a3.75 3.75 0 000 4.975zM13.55 8a5 5 0 100 10 5 5 0 000-10zm0 7.5a2.5 2.5 0 110-5 2.5 2.5 0 010 5z"
+                fill="#000"
+                stroke="#C1D4F9"
+              />
+            </Svg>
+          </TouchableOpacity>
+        </View>
+      </LinearGradient>
+    </>
+  );
+};
+
+export default HeadSection;
+
+const styles = StyleSheet.create({
+  banner: {
+    height: 350,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    paddingHorizontal: 15,
+    display: "flex",
+    flexDirection: "column",
+    gap: 20,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  headText: {
+    marginTop: 10,
+    fontSize: 22,
+    fontWeight: 500,
+    color: "#ffffff",
+    fontFamily: "Saira-Medium",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    justifyContent: "space-between",
+  },
+  center: {
+    flex: 1,
+    gap: 7,
+    alignItems: "center",
+  },
+  avatar: {
+    width: 90,
+    height: 90,
+    borderRadius: 100,
+  },
+  name: {
+    fontSize: 14,
+    paddingVertical: 5,
+    paddingHorizontal: 7,
+    borderRadius: 10000,
+    color: "#FFFFFF",
+    fontFamily: "Saira-Regular",
+    backgroundColor: "#285CE8",
+  },
+  phone: {
+    fontSize: 14,
+    color: "#FFFFFF",
+    fontFamily: "Saira-Regular",
+  },
+  amountBar: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 25,
+    gap: 5,
+  },
+  barBlock: {
+    gap: 5,
+    alignItems: "center",
+    width: 100,
+    paddingHorizontal: 5,
+    justifyContent: "flex-end",
+  },
+  barNumber: {
+    fontSize: 18,
+    color: "#FFFFFF",
+    fontFamily: "Saira-Medium",
+  },
+  barText: {
+    fontSize: 14,
+    color: "#FFFFFF",
+    fontFamily: "Saira-Medium",
+  },
+  sideBtn: {
+    gap: 10,
+    position: "absolute",
+    top: "10%",
+    right: 15,
+  },
+  notificationButton: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 50,
+    height: 50,
+    borderRadius: 1000,
+    backgroundColor: "#FFFFFFB2",
+  },
+});
